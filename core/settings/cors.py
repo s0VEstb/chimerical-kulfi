ed7dde23d3e091b875b91683155440f52c311814
s0VEstb <attokurov090506@gmail.com>
2024-12-16 12:07:52 +0600

    changed corst

--- core/settings/cors.py
+++ core/settings/cors.py
@@ -1,13 +1,4 @@
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:3080",
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:4040",
-    "http://localhost:5173",
-    "http://localhost:8000",
-]
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000', "http://localhost:5173", "http://localhost:8000",]
-LOCAL_HOST = ['*']
-ALLOWED_HOSTS = LOCAL_HOST + CORS_ALLOWED_ORIGINS
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ['*']
+CSRF_COOKIE_SECURE = False
