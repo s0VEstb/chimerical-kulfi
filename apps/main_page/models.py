d75cdb492eb727c3202bdbd0ebfc6c665c31979d
s0VEstb <attokurov090506@gmail.com>
2024-12-16 13:36:40 +0600

    changed url field

--- apps/main_page/models.py
+++ apps/main_page/models.py
@@ -34,7 +34,7 @@ class Category(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = models.URLField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
